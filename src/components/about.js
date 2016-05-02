86559804d4272f82c0ff6426fadaad9206747dfc
Tim Knight <timmilesknight@gmail.com>
2016-05-02 16:53:20 +0100

    Add fadeIn animation to about content

--- src/components/about.js
+++ src/components/about.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div id="about">This site was built by Tim Knight as a project to learn React.js. It uses a NodeJS & ExpressJS backend API and React.js on the front end. The react side uses
+    <div id="about" className="animated fadeIn">This site was built by Tim Knight as a project to learn React.js. It uses a NodeJS & ExpressJS backend API and React.js on the front end. The react side uses
     simple stateless functional components as well as components built using ES6 classes. The project is deliberatly simple and doesn't use a Redux architecture. The API has limited validation and is deliberatly simple.</div>
   )
 }
