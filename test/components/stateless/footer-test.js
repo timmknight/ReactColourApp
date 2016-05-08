ed3ca5fb1453db6fb1a64fd1bf65c3e04450fb8e
Tim Knight <timmilesknight@gmail.com>
2016-05-08 17:34:30 +0100

    Footer tests

--- test/components/stateless/footer-test.js
+++ test/components/stateless/footer-test.js
@@ -4,14 +4,33 @@ import React from 'react';
 import sd from 'skin-deep';
 
 describe('Footer', () => {
+
     let component;
 
     beforeEach(() => {
         component = sd.shallowRender(<Footer />);
     });
 
-    it('', () => {
-        //expect
+    it('Should be of type footer', () => {
+        expect(component.type).to.eql('footer');
+    });
+
+    it('Should contain a child of type div', () => {
+        expect(component.props.children.type).to.eql('div');
+    });
+
+    describe('Child', () => {
+        
+        let child;
+        
+        beforeEach(() => {
+            child = component.props.children;
+        });
+        
+        it('The child div should contain the text', () => {
+            expect(child.props.children).to.eql('Built by Tim Knight');
+        });
+
     });
 
 });
